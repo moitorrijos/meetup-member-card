d4209b4c48b9f33ddb71468423aafb291092e809
moitorrijos <juanmtorrijos@gmail.com>
2020-06-21 19:00:45 -0500

    Change plugin name.

--- plugin.php
+++ plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: Team Member Block CGB
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: New Team Member Block for Gutenberg.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: Meetup Team Member Block
+ * Plugin URI: https://github.com/moitorrijos.com/meetup-member-card/
+ * Description: Bloque Tipo Tarjeta para el nuevo editor de WordPress.
+ * Author: moitorrijos, ahmadawais
+ * Author URI: https://moitorrijos.com/
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
